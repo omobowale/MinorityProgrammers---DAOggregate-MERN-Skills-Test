6db9095491556afa6b99fff79310c8d769ccfbd8
Omobowale Otuyiga <omobowale.otuyiga@ab-mfbnigeria.com>
2021-08-16 17:20:08 +0100

    minor changes

--- app.js
+++ app.js
@@ -26,14 +26,16 @@ app.use("/", daoRouter);
 
 app.use("/socialmedia", socialmediaRouter);
 
+if(process.env.NODE_ENV === "production"){
+  //serving static pages
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-//serving static pages
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
 
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+}
 
 //listen at the port
 const PORT  = process.env.PORT || 3009
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
